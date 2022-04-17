f4501f765ec016db80abc94b5f6799549059d239
DragonWF <plarisanmarc@gmail.com>
2022-04-18 02:02:42 +0800

    Added main functions and main variables

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,8 +1,31 @@
 import Command from "../utils/command.js";
+import MessageLogger from "../utils/message_logger.js";
 
 class SnipeCommand extends Command {
   constructor() {
     super();
+    this.deletedMessage = null;
+    this.editedMessage = { before: null, after: null };
+  }
+
+  storeDeletedMessage(message) {
+    if (MessageLogger.validateMessageContent(message.content)) {
+      return;
+    }
+  }
+
+  storeEditedMessage(before, after) {
+    if (MessageLogger.validateMessageContent(after.content)) {
+      return;
+    }
+  }
+
+  snipeDeletedMessage() {
+    return;
+  }
+
+  snipeEditedMessage() {
+    return;
   }
 }
 
