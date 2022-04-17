7a48928fe56865eb0dd54654c35697060391f712
DragonWF <plarisanmarc@gmail.com>
2022-04-17 22:12:03 +0800

    Constructed main logic for responding

--- utils/keyword-responder.js
+++ utils/keyword-responder.js
@@ -3,27 +3,44 @@ import fs from "fs";
 const data = JSON.parse(fs.readFileSync("../config/keywords.json"));
 
 class KeyWordResponder {
-  checkForKeyWord(message) {
+  static checkForKeyWord(message) {
     for (let word of message.content.split(" ")) {
       for (let object of data) {
         if (object.keywords.includes(word)) {
-          this.respondToMessage(message, data);
+          this.respondToMessage(message, object);
           return;
         }
       }
     }
   }
 
-  chooseResponse(keywordObject, reaction) {
-      
+  static isReactionResponse(keywordObject) {
+    const hasPhrases = keywordObject.responses.phrases.length;
+    const hasEmojis = keywordObject.responses.emojis.length;
+    if (keywordObject.hasReaction) {
+      if (hasPhrases && hasEmojis) {
+        const chance = Math.floor(Math.random() * 2) + 1;
+        if (chance == 1) return true;
+        else return false;
+      }
+      if (hasPhrases) return false;
+      if (hasEmojis) return true;
+    }
+    return false;
   }
 
-  respondToMessage(message, keywordObject) {
-    if (keywordObject.hasReaction) {
-      const chance = Math.floor(Math.random() * 2) + 1;
-      if (chance == 1) {
-        if (keywordObject.reply) message.reply();
-      }
-    }
+  static chooseResponse(keywordObject, reaction) {
+    const responses = reaction
+      ? keywordObject.responses.emojis
+      : keywordObject.responses.phrases;
+    return responses[Math.floor(Math.random() * responses.length)];
+  }
+
+  static respondToMessage(message, keywordObject) {
+    const isReaction = this.isReactionResponse(keywordObject);
+    const response = chooseResponse(keywordObject, isReaction);
+    if (isReaction) message.react(response);
+    else if (keywordObject.useReply) message.reply(response);
+    else message.channel.send(response);
   }
 }
