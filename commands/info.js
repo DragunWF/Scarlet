2298b1af1906268d84938426c884021b6f342403
DragonWF <plarisanmarc@gmail.com>
2022-04-17 23:42:42 +0800

    Updated embed image and description

--- commands/info.js
+++ commands/info.js
@@ -1,8 +1,9 @@
 import Command from "../utils/command.js";
 
 const description = `
-Hello! This is a Discord Bot.
-`; // Change description
+Hello! This is a discord bot written in JavaScript. Made by \`DragonWF#9321\`
+This bot is specifically made for personal use and nothing else.
+`;
 
 class InfoCommand extends Command {
   constructor() {
@@ -13,14 +14,14 @@ class InfoCommand extends Command {
     const embedOutput = new object.MessageEmbed()
       .setColor(object.mainColor)
       .setAuthor({
-        // Feel free to change for people who are planning to use this template
         name: "DragonWF#9321",
-        iconURL: "https://cdn.discordapp.com/avatars/408972598798450688/617053b744713e4a211c1e119ec46ab4.webp",
+        iconURL:
+          "https://cdn.discordapp.com/avatars/408972598798450688/617053b744713e4a211c1e119ec46ab4.webp",
       })
       .setTitle("Bot Information")
       .setDescription(description)
       .setImage(
-        "https://cdn.discordapp.com/avatars/939435166034722827/73ead4510886bc2ecfd08b7147b07d17.webp"
+        "https://cdn.discordapp.com/avatars/965201825928196098/c3714dff1fa2f6f32b91bf802de82de5.webp"
       )
       .setFooter({ text: "Have a nice day!" });
     message.channel.send({ embeds: [embedOutput] });
