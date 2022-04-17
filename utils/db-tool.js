f6823a9c4650ab2e01723bf4ef3fcb81b0a80b10
DragonWF <plarisanmarc@gmail.com>
2022-04-17 22:13:33 +0800

    Added variable for connection to database

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -1,1 +1,19 @@
+import "dovenv/config";
 import mysql from "mysql";
+
+const db = mysql.createPool({
+  connectionLimit: 5,
+  aquireTimeout: 10000,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+class DatabaseTool {
+  static insertCreatedMessage(message) {
+    //
+  }
+}
+
+export default DatabaseTool;
