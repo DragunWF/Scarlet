3c4262c4a244f36daf5436d33eade9124969517d
DragonWF <plarisanmarc@gmail.com>
2022-04-18 02:29:35 +0800

    Added main logic for storing messages and responses for no snipes

--- commands/snipe.js
+++ commands/snipe.js
@@ -4,28 +4,52 @@ import MessageLogger from "../utils/message_logger.js";
 class SnipeCommand extends Command {
   constructor() {
     super();
+    this.noSnipeResponses = [
+      "Better luck next time lad, there is nothing to snipe...",
+      "Sorry, but there is nothing to snipe.",
+      "Well that is unfortunate, there is nothing to snipe.",
+      "Sorry but I can't snipe anything when I have nothing recorded within my magic.",
+    ];
     this.deletedMessage = null;
     this.editedMessage = { before: null, after: null };
   }
 
   storeDeletedMessage(message) {
-    if (MessageLogger.validateMessageContent(message.content)) {
-      return;
-    }
+    if (MessageLogger.validateMessageContent(message.content))
+      this.deletedMessage = message;
   }
 
   storeEditedMessage(before, after) {
     if (MessageLogger.validateMessageContent(after.content)) {
-      return;
+      this.editedMessage.after = after;
+      this.editedMessage.before = before;
     }
   }
 
-  snipeDeletedMessage() {
-    return;
+  snipeDeletedMessage(object, message) {
+    if (object.deletedMessage) {
+    //   const embed = new object.MessageEmbed()
+    //     .setColor(object.mainColor)
+    //     .setAuthor();
+    } else {
+      message.channel.reply(
+        object.noSnipeResponses[
+          Math.floor(Math.random() * object.noSnipeResponses.length)
+        ]
+      );
+    }
   }
 
-  snipeEditedMessage() {
-    return;
+  snipeEditedMessage(object, message, snipeBefore = false) {
+    if (object.editedMessage.before && object.editedMessage.after) {
+      return;
+    } else {
+      message.channel.reply(
+        object.noSnipeResponses[
+          Math.floor(Math.random() * object.noSnipeResponses.length)
+        ]
+      );
+    }
   }
 }
 
