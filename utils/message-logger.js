a0904ac256dfeba26e0ab81eb9e0ed5f7b9f7f7e
DragonWF <plarisanmarc@gmail.com>
2022-04-18 22:20:56 +0800

    Fixed message content validation bug

--- utils/message-logger.js
+++ utils/message-logger.js
@@ -2,18 +2,19 @@ import DatabaseTool from "./db-tool.js";
 
 class MessageLogger {
   static validateMessageContent(content) {
-    if (!content.length > 1) return false;
+    if (!(content.length > 1)) return false;
     if (content.length < 10) {
       const uniqueCount = [...new Set(content.split(""))].filter((element) => {
         if (element === " ") return element;
       }).length;
-      if (!uniqueCount > 1) return false;
+      if (!(uniqueCount > 1)) return false;
     }
+    console.log("passed");
     return true;
   }
 
   static logCreatedMessage(message) {
-    if (this.validateMessageContent) {
+    if (this.validateMessageContent(message.content)) {
       DatabaseTool.insertCreatedMessage(message);
     }
   }
