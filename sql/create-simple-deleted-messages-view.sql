1df677a6d9efbd607544a115389b7d7a7244f895
DragonWF <plarisanmarc@gmail.com>
2022-04-18 23:35:48 +0800

    Created view for simple deleted messages logs

---
+++ sql/create-simple-deleted-messages-view.sql
@@ -0,0 +1,11 @@
+USE scarlet_db;
+
+CREATE OR REPLACE VIEW simple_deleted_messages AS
+SELECT
+	author_tag AS name,
+    message_content AS content,
+    date_deleted,
+    time_deleted
+FROM deleted_messages;
+
+SELECT * FROM simple_deleted_messages;
