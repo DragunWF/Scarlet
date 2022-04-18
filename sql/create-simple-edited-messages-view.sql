6d2631ed5ae44500ea305bfbed547f483e5f1d61
DragonWF <plarisanmarc@gmail.com>
2022-04-18 23:35:39 +0800

    Created view for simple edited message logs

---
+++ sql/create-simple-edited-messages-view.sql
@@ -0,0 +1,12 @@
+USE scarlet_db;
+
+CREATE OR REPLACE VIEW simple_edited_messages AS
+SELECT
+	author_tag AS name,
+    before_edit_content AS before_edit,
+    after_edit_content AS after_edit,
+    date_edited,
+    time_edited
+FROM edited_messages;
+
+SELECT * FROM simple_edited_messages;
