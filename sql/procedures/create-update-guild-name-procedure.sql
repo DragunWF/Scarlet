7bb92a1929d61835a2695ccb1dec03c2c1046388
DragonWF <plarisanmarc@gmail.com>
2022-04-19 19:19:34 +0800

    Created procedure for guild name updates

---
+++ sql/procedures/create-update-guild-name-procedure.sql
@@ -0,0 +1,17 @@
+USE scarlet_db;
+
+DROP PROCEDURE IF EXISTS update_guild_name;
+
+DELIMITER $$
+CREATE PROCEDURE update_guild_name
+(
+	guild_id BIGINT,
+    new_name VARCHAR(100)
+)
+BEGIN
+	UPDATE guilds g
+    SET g.guild_name = new_name
+    WHERE g.guild_id = guild_id;
+END $$
+
+DELIMITER ;
