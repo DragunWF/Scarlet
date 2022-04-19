02bb89bb949dd83d576d0f10188f1f48c0ebfdd2
DragonWF <plarisanmarc@gmail.com>
2022-04-19 22:22:25 +0800

    Fixed syntax

--- sql/schema/create-scarlet-db.sql
+++ sql/schema/create-scarlet-db.sql
@@ -1,4 +1,4 @@
--- Create Database (Without create schema)
+-- Create Database (Without schema creation)
 -- The purpose is to just fill an empty schema
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -6,117 +6,117 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 ----------------------------------------------------
------- Create all tables
+-- Create all tables
 ----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`guilds` (
-  `guild_id` BIGINT NOT NULL,
-  `guild_name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`guild_id`))
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.guilds (
+  guild_id BIGINT NOT NULL,
+  guild_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (guild_id))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`channels` (
-  `guild_id` BIGINT NOT NULL,
-  `channel_id` BIGINT NOT NULL,
-  `channel_name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`channel_id`),
-  INDEX `fk_channels_guilds_idx` (`guild_id` ASC) VISIBLE,
-  CONSTRAINT `fk_channels_guilds`
-    FOREIGN KEY (`guild_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`guilds` (`guild_id`)
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.channels (
+  guild_id BIGINT NOT NULL,
+  channel_id BIGINT NOT NULL,
+  channel_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (channel_id),
+  INDEX fk_channels_guilds_idx (guild_id ASC) VISIBLE,
+  CONSTRAINT fk_channels_guilds
+    FOREIGN KEY (guild_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.guilds (guild_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`users` (
-  `author_id` BIGINT NOT NULL,
-  `author_tag` VARCHAR(37) NOT NULL,
-  PRIMARY KEY (`author_id`))
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.users (
+  author_id BIGINT NOT NULL,
+  author_tag VARCHAR(37) NOT NULL,
+  PRIMARY KEY (author_id))
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`messages` (
-  `message_id` BIGINT NOT NULL,
-  `channel_id` BIGINT NOT NULL,
-  `guild_id` BIGINT NOT NULL,
-  `author_id` BIGINT NOT NULL,
-  `message_content` VARCHAR(2000) NOT NULL,
-  `date_sent` DATE NOT NULL,
-  `time_sent` TIME NOT NULL,
-  PRIMARY KEY (`message_id`),
-  INDEX `fk_messages_channels1_idx` (`channel_id` ASC) VISIBLE,
-  INDEX `fk_messages_guilds1_idx` (`guild_id` ASC) VISIBLE,
-  INDEX `fk_messages_users1_idx` (`author_id` ASC) VISIBLE,
-  CONSTRAINT `fk_messages_channels1`
-    FOREIGN KEY (`channel_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`channels` (`channel_id`)
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.messages (
+  message_id BIGINT NOT NULL,
+  channel_id BIGINT NOT NULL,
+  guild_id BIGINT NOT NULL,
+  author_id BIGINT NOT NULL,
+  message_content VARCHAR(2000) NOT NULL,
+  date_sent DATE NOT NULL,
+  time_sent TIME NOT NULL,
+  PRIMARY KEY (message_id),
+  INDEX fk_messages_channels1_idx (channel_id ASC) VISIBLE,
+  INDEX fk_messages_guilds1_idx (guild_id ASC) VISIBLE,
+  INDEX fk_messages_users1_idx (author_id ASC) VISIBLE,
+  CONSTRAINT fk_messages_channels1
+    FOREIGN KEY (channel_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.channels (channel_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_guilds1`
-    FOREIGN KEY (`guild_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`guilds` (`guild_id`)
+  CONSTRAINT fk_messages_guilds1
+    FOREIGN KEY (guild_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.guilds (guild_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_users1`
-    FOREIGN KEY (`author_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`users` (`author_id`)
+  CONSTRAINT fk_messages_users1
+    FOREIGN KEY (author_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.users (author_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`deleted_messages` (
-  `message_id` BIGINT NOT NULL,
-  `guild_id` BIGINT NOT NULL,
-  `channel_id` BIGINT NOT NULL,
-  `author_id` BIGINT NOT NULL,
-  `message_content` VARCHAR(2000) NOT NULL,
-  `date_deleted` DATE NOT NULL,
-  `time_deleted` TIME NOT NULL,
-  PRIMARY KEY (`message_id`),
-  INDEX `fk_deleted_messages_guilds1_idx` (`guild_id` ASC) VISIBLE,
-  INDEX `fk_deleted_messages_channels1_idx` (`channel_id` ASC) VISIBLE,
-  INDEX `fk_deleted_messages_users1_idx` (`author_id` ASC) VISIBLE,
-  CONSTRAINT `fk_deleted_messages_guilds1`
-    FOREIGN KEY (`guild_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`guilds` (`guild_id`)
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.deleted_messages (
+  message_id BIGINT NOT NULL,
+  guild_id BIGINT NOT NULL,
+  channel_id BIGINT NOT NULL,
+  author_id BIGINT NOT NULL,
+  message_content VARCHAR(2000) NOT NULL,
+  date_deleted DATE NOT NULL,
+  time_deleted TIME NOT NULL,
+  PRIMARY KEY (message_id),
+  INDEX fk_deleted_messages_guilds1_idx (guild_id ASC) VISIBLE,
+  INDEX fk_deleted_messages_channels1_idx (channel_id ASC) VISIBLE,
+  INDEX fk_deleted_messages_users1_idx (author_id ASC) VISIBLE,
+  CONSTRAINT fk_deleted_messages_guilds1
+    FOREIGN KEY (guild_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.guilds (guild_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_deleted_messages_channels1`
-    FOREIGN KEY (`channel_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`channels` (`channel_id`)
+  CONSTRAINT fk_deleted_messages_channels1
+    FOREIGN KEY (channel_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.channels (channel_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_deleted_messages_users1`
-    FOREIGN KEY (`author_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`users` (`author_id`)
+  CONSTRAINT fk_deleted_messages_users1
+    FOREIGN KEY (author_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.users (author_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `bkwtrdqkijpbr852tmj3`.`edited_messages` (
-  `message_id` BIGINT NOT NULL,
-  `guild_id` BIGINT NOT NULL,
-  `channel_id` BIGINT NOT NULL,
-  `author_id` BIGINT NOT NULL,
-  `before_edit_content` VARCHAR(2000) NOT NULL,
-  `after_edit_content` VARCHAR(2000) NOT NULL,
-  `date_edited` DATE NOT NULL,
-  `time_edited` TIME NOT NULL,
-  PRIMARY KEY (`message_id`),
-  INDEX `fk_edited_messages_guilds1_idx` (`guild_id` ASC) VISIBLE,
-  INDEX `fk_edited_messages_channels1_idx` (`channel_id` ASC) VISIBLE,
-  INDEX `fk_edited_messages_users1_idx` (`author_id` ASC) VISIBLE,
-  CONSTRAINT `fk_edited_messages_guilds1`
-    FOREIGN KEY (`guild_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`guilds` (`guild_id`)
+CREATE TABLE IF NOT EXISTS bkwtrdqkijpbr852tmj3.edited_messages (
+  message_id BIGINT NOT NULL,
+  guild_id BIGINT NOT NULL,
+  channel_id BIGINT NOT NULL,
+  author_id BIGINT NOT NULL,
+  before_edit_content VARCHAR(2000) NOT NULL,
+  after_edit_content VARCHAR(2000) NOT NULL,
+  date_edited DATE NOT NULL,
+  time_edited TIME NOT NULL,
+  PRIMARY KEY (message_id),
+  INDEX fk_edited_messages_guilds1_idx (guild_id ASC) VISIBLE,
+  INDEX fk_edited_messages_channels1_idx (channel_id ASC) VISIBLE,
+  INDEX fk_edited_messages_users1_idx (author_id ASC) VISIBLE,
+  CONSTRAINT fk_edited_messages_guilds1
+    FOREIGN KEY (guild_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.guilds (guild_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_edited_messages_channels1`
-    FOREIGN KEY (`channel_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`channels` (`channel_id`)
+  CONSTRAINT fk_edited_messages_channels1
+    FOREIGN KEY (channel_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.channels (channel_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_edited_messages_users1`
-    FOREIGN KEY (`author_id`)
-    REFERENCES `bkwtrdqkijpbr852tmj3`.`users` (`author_id`)
+  CONSTRAINT fk_edited_messages_users1
+    FOREIGN KEY (author_id)
+    REFERENCES bkwtrdqkijpbr852tmj3.users (author_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -126,7 +126,7 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 ----------------------------------------------------
------- Create all views
+-- Create all views
 ----------------------------------------------------
 CREATE OR REPLACE VIEW simple_messages AS
 SELECT
@@ -160,7 +160,7 @@ JOIN users u USING (author_id)
 ORDER BY time_edited, date_edited DESC;
 
 ----------------------------------------------------
------- Create all stored procedures
+-- Create all stored procedures
 ----------------------------------------------------
 DELIMITER $$
 
@@ -220,7 +220,7 @@ BEGIN
 END $$
 
 ----------------------------------------------------
------- Create all events
+-- Create all events
 ----------------------------------------------------
 CREATE EVENT monthly_clear_logs
 ON SCHEDULE
@@ -233,13 +233,10 @@ END $$
 
 DELIMITER ;
 
-----------------------------------------------------
------- Alter character to accept emojis
-----------------------------------------------------
+-- Alter table character sets to accept emojis
 ALTER TABLE messages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 ALTER TABLE deleted_messages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 ALTER TABLE edited_messages CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-
 ALTER TABLE users CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 ALTER TABLE channels CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 ALTER TABLE guilds CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
