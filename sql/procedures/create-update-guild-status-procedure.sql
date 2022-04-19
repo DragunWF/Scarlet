d22b5046e1bb176605674973d769cdc4d299dc4f
DragonWF <plarisanmarc@gmail.com>
2022-04-19 19:20:55 +0800

    Created procedure for updating guild status

---
+++ sql/procedures/create-update-guild-status-procedure.sql
@@ -0,0 +1,19 @@
+USE scarlet_db;
+
+DROP PROCEDURE IF EXISTS update_guild_status;
+
+DELIMITER $$
+CREATE PROCEDURE update_guild_status
+(
+	guild_id BIGINT
+)
+BEGIN
+	UPDATE guilds g
+    SET status = "deleted"
+    WHERE g.guild_id = guild_id;
+    
+    DELETE FROM channels
+    WHERE g.guild_id = guild_id;
+END $$
+
+DELIMITER ;
