ec241ada7ef9d80f4fa715b9b168331b159c4b59
DragonWF <plarisanmarc@gmail.com>
2022-04-20 01:24:28 +0800

    Updated event script

--- sql/events/create-monthly-clear-logs-event.sql
+++ sql/events/create-monthly-clear-logs-event.sql
@@ -1,13 +1,15 @@
 -- For clearing the logs every month and to save space...
 
+DROP EVENT IF EXISTS monthly_clear_logs;
+
 DELIMITER $$
 CREATE EVENT monthly_clear_logs
 ON SCHEDULE
 	EVERY 1 MONTH STARTS "2022-01-01"
 DO BEGIN
-	TRUNCATE messages;
-    TRUNCATE deleted_messages;
-    TRUNCATE edited_messages;
+  DELETE FROM messages;
+  DELETE FROM deleted_messages;
+  DELETE FROM edited_messages;
 END $$
 
 DELIMITER ;
