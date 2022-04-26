ac65c4dfd7289b4143754f6d28592679df5449d5
DragonWF <plarisanmarc@gmail.com>
2022-04-26 16:30:34 +0800

    Removed hardcoded values and implemented private class variables

--- commands/info.js
+++ commands/info.js
@@ -1,30 +1,36 @@
+import fs from "fs";
 import Command from "../utils/command.js";
 
-const description = `
-Hello! This is a discord bot written in JavaScript. Made by \`DragonWF#9321\`.
-This bot is specifically made for personal use and nothing else.
-`;
-
 class InfoCommand extends Command {
+  #author;
+  #description;
+
   constructor() {
     super();
+    this.#author = JSON.parse(
+      fs.readFileSync("./config/bot.json")
+    )[0].creatorTag;
+    this.#description = `
+Hello! This is a discord bot written in JavaScript. Made by \`${this.#author}\`.
+This bot is specifically made for personal use and nothing else.
+    `;
   }
 
-  getBotInformation(object, message) {
-    const embedOutput = new object.MessageEmbed()
-      .setColor(object.mainColor)
+  executeCommand(message) {
+    const embed = new this.MessageEmbed()
+      .setColor(this.mainColor)
       .setAuthor({
-        name: "DragonWF#9321",
+        name: this.#author,
         iconURL:
           "https://cdn.discordapp.com/avatars/408972598798450688/617053b744713e4a211c1e119ec46ab4.webp",
       })
       .setTitle("Bot Information")
-      .setDescription(description)
+      .setDescription(this.#description)
       .setImage(
         "https://cdn.discordapp.com/avatars/965201825928196098/c3714dff1fa2f6f32b91bf802de82de5.webp"
       )
       .setFooter({ text: "Have a nice day!" });
-    message.channel.send({ embeds: [embedOutput] });
+    message.channel.send({ embeds: [embed] });
   }
 }
 
