a9b562748db83bc80013bd40ad57b402b0d4ae17
DragonWF <plarisanmarc@gmail.com>
2022-04-26 17:04:34 +0800

    Renamed file, separated send and set to two different classes, and implemented private variables and methods

---
+++ commands/set-and-send.js
@@ -0,0 +1,68 @@
+import Command from "../utils/command.js";
+
+export class SendMessageCommand extends Command {
+  #channel;
+
+  constructor() {
+    super();
+    this.#channel = null;
+  }
+
+  executeCommand(message, content) {
+    try {
+      if (this.#channel) this.#channel.send(content.join(" "));
+      else {
+        const responses = [
+          "A channel has not been set up yet master...",
+          "Master, you need to set up a channel first before I can talk",
+          "A channel needs to be set up first master before I can talk",
+        ];
+        message.channel.send(
+          responses[Math.floor(Math.random() * responses.length)]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+      const errorResponses = [
+        "I have been rudely interrupted master! I couldn't talk",
+        "Sorry master, but it seems like I have ran into a problem",
+        "Master! I have encountered myself into an error",
+      ];
+      message.reply(
+        errorResponses[Math.floor(Math.random() * errorResponses.length)]
+      );
+    }
+  }
+}
+
+export class SetChannelCommand extends Command {
+  constructor() {
+    super();
+  }
+
+  executeCommand(message, client, channelId) {
+    if (this.channel && this.channel.id === channelId[0])
+      message.channel.send("I've already been set up there master");
+    else {
+      const responses = this.channel
+        ? [
+            "Time for me to move to a different area",
+            "Well, time to travel again I guess",
+            "Onwards I go, to adventure!",
+            "Another adventure, another tale to be unwoven...",
+            "Travelling travelling, here I go hehe",
+          ]
+        : [
+            "Oh this will be fun hehe",
+            "Ah yes, it is time to start our fun",
+            "Time for another session hehe...",
+          ];
+
+      this.channel = client.channels.cache.get(channelId[0]);
+      if (!this.channel)
+        message.channel.send("That's an invalid input master!");
+      else
+        message.reply(responses[Math.floor(Math.random() * responses.length)]);
+    }
+  }
+}
