3b84703af6c43ca9ab731c4ea0d1bcadb3e662e5
DragonWF <plarisanmarc@gmail.com>
2022-04-26 16:29:48 +0800

    Implemented private class variables and private methods

--- commands/help.js
+++ commands/help.js
@@ -1,35 +1,38 @@
 import Command from "../utils/command.js";
 
 class HelpCommand extends Command {
+  #prefix;
+  #commandList;
+
   constructor() {
     super();
-    this.prefix = null;
-    this.commandList = [];
+    this.#prefix = JSON.parse(fs.readFileSync("./config/bot.json"))[0].prefix;
+    this.#commandList = [];
+  }
+
+  executeCommand(message) {
+    const embed = new this.MessageEmbed()
+      .setColor(this.mainColor)
+      .setTitle("List of Commands")
+      .setDescription(this.#concatenateCommands())
+      .setFooter({ text: "Have fun!" });
+    message.channel.send({ embeds: [embed] });
   }
 
   fillCommandList(commands) {
     for (let command of commands)
-      this.commandList.push({ name: command.name, hasArgs: command.hasArgs });
+      this.#commandList.push({ name: command.name, hasArgs: command.hasArgs });
   }
 
-  concatenateCommands() {
+  #concatenateCommands() {
     let withArgs = "";
     let noArgs = "";
-    for (let command of this.commandList) {
+    for (let command of this.#commandList) {
       if (command.hasArgs)
-        withArgs += `- \`${this.prefix}${command.name} <argument>\`\n`;
-      else noArgs += `- \`${this.prefix}${command.name}\`\n`;
+        withArgs += `- \`${this.#prefix}${command.name} <argument>\`\n`;
+      else noArgs += `- \`${this.#prefix}${command.name}\`\n`;
     }
     return `**Regular Commands:**\n${noArgs}\n**Commands With Arguments:**\n${withArgs}`;
-  }
-
-  processHelpCommand(object, message) {
-    const embed = new object.MessageEmbed()
-      .setColor(object.mainColor)
-      .setTitle("List of commands")
-      .setDescription(object.concatenateCommands())
-      .setFooter({ text: "Have fun!" });
-    message.channel.send({ embeds: [embed] });
   }
 }
 
