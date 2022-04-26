9c3c475f116bd9f8ee7ad7fb828789a9b1dfaec3
DragonWF <plarisanmarc@gmail.com>
2022-04-26 16:34:26 +0800

    Renewed command processing system with polymorphism, cleaned code, and implemented private class variables

--- utils/processor.js
+++ utils/processor.js
@@ -4,52 +4,89 @@ import PingCommand from "../commands/ping.js";
 import HelpCommand from "../commands/help.js";
 import InfoCommand from "../commands/info.js";
 import CryptoCommand from "../commands/crypto.js";
-import SnipeCommand from "../commands/snipe.js";
+import { SnipeCommand, EditSnipeCommand } from "../commands/snipes.js";
 import ChatCommand from "../commands/chat.js";
 
-const settings = JSON.parse(fs.readFileSync("./config/bot.json"))[0];
-
-const ping = new PingCommand();
-const help = new HelpCommand();
-const info = new InfoCommand();
-const crypto = new CryptoCommand();
-const snipe = new SnipeCommand();
 const chat = new ChatCommand();
 
-// The purpose of command executions is to map command objects with a function to call
-const commands = JSON.parse(fs.readFileSync("./config/commands.json"));
-const commandExecutions = [
-  { name: "ping", call: ping.getBotLatency, object: ping },
-  { name: "help", call: help.processHelpCommand, object: help },
-  { name: "info", call: info.getBotInformation, object: info },
-  { name: "crypto", call: crypto.sendCryptoData, object: crypto },
-  { name: "snipe", call: snipe.snipeDeletedMessage, object: snipe },
-  { name: "esnipe", call: snipe.snipeEditedMessage, object: snipe },
-  { name: "chat", call: chat.sendMessage, object: chat },
-  { name: "channel", call: chat.setChannel, object: chat },
-];
-
 class CommandProcessor {
+  static #commands = JSON.parse(fs.readFileSync("./config/commands.json"));
+  static #commandExecutions = [
+    { name: "ping", object: new PingCommand() },
+    { name: "help", object: new HelpCommand() },
+    { name: "info", object: new InfoCommand() },
+    { name: "crypto", object: new CryptoCommand() },
+    { name: "snipe", object: new SnipeCommand() },
+    { name: "esnipe", object: new EditSnipeCommand() },
+    { name: "chat", object: chat },
+    { name: "channel", object: chat },
+  ];
+  static #settings = JSON.parse(fs.readFileSync("./config/bot.json"))[0];
+  static #snipeCommandIndex;
+  static #esnipeCommandIndex;
+
   static onReady() {
-    ping.prefix = settings.prefix;
-    help.prefix = settings.prefix;
-    help.fillCommandList(commands);
+    let helpCommandIndex;
+    for (let i = 0; i < this.#commandExecutions.length; i++) {
+      switch (this.#commandExecutions[i].name) {
+        case "help":
+          helpCommandIndex = i;
+          break;
+        case "snipe":
+          this.#snipeCommandIndex = i;
+          break;
+        case "esnipe":
+          this.#esnipeCommandIndex = i;
+          break;
+      }
+    }
+
+    this.#commandExecutions[helpCommandIndex].object.fillCommandList(
+      this.#commands
+    );
     this.mapCommandExecutions();
   }
 
-  static mapCommandExecutions() {
-    commands.sort((a, b) => a.name.localeCompare(b.name));
-    commandExecutions.sort((a, b) => a.name.localeCompare(b.name));
+  static processCommand(command, client) {
+    const [commandName, ...args] = command.content
+      .trim()
+      .substring(this.#settings.prefix.length)
+      .split(/\s+/);
 
-    for (let i = 0; i < commands.length; i++) {
-      commands[i].execution = commandExecutions[i].call;
-      commands[i].object = commandExecutions[i].object;
+    const parameters = [];
+    for (let cmd of this.#commands) {
+      if (cmd.alias.includes(commandName.toLowerCase())) {
+        if (
+          cmd.rulerCommand &&
+          command.author.id !== this.#settings.users.master
+        ) {
+          this.#rulerCommandOnly(command);
+          break;
+        } else {
+          // parameters.push(cmd.object);
+          parameters.push(command);
+          if (cmd.hasClientObject) parameters.push(client);
+          if (cmd.hasArgs) parameters.push(args);
+          cmd.object.executeCommand(...parameters);
+          break;
+        }
+      }
     }
   }
 
+  static mapCommandExecutions() {
+    this.#commands.sort((a, b) => a.name.localeCompare(b.name));
+    this.#commandExecutions.sort((a, b) => a.name.localeCompare(b.name));
+
+    for (let i = 0; i < this.#commands.length; i++)
+      this.#commands[i].object = this.#commandExecutions[i].object;
+  }
+
   static modifySnipeCommand(isDeletedMessage, message) {
+    const snipe = this.#commandExecutions[this.#snipeCommandIndex].object;
+    const esnipe = this.#commandExecutions[this.#esnipeCommandIndex].object;
     if (isDeletedMessage) snipe.storeDeletedMessage(message);
-    else snipe.storeEditedMessage(message.before, message.after);
+    else esnipe.storeEditedMessage(message.before, message.after);
   }
 
   static #rulerCommandOnly(message) {
@@ -61,30 +98,6 @@ class CommandProcessor {
       `Hey ${message.author.name}, You can't make me run this command...`,
     ];
     message.reply(responses[Math.floor(Math.random() * responses.length)]);
-  }
-
-  static processCommand(command, client) {
-    const [commandName, ...args] = command.content
-      .trim()
-      .substring(settings.prefix.length)
-      .split(/\s+/);
-
-    const parameters = [];
-    for (let cmd of commands) {
-      if (cmd.alias.includes(commandName.toLowerCase())) {
-        if (cmd.rulerCommand && command.author.id !== settings.users.master) {
-          this.#rulerCommandOnly(command);
-          break;
-        } else {
-          parameters.push(cmd.object);
-          parameters.push(command);
-          if (cmd.hasClientObject) parameters.push(client);
-          if (cmd.hasArgs) parameters.push(args);
-          cmd.execution(...parameters);
-          break;
-        }
-      }
-    }
   }
 }
 
