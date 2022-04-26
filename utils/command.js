b2cb900543b228e4cd8463cd54c30cd8f083cea2
DragonWF <plarisanmarc@gmail.com>
2022-04-26 16:31:15 +0800

    Replaced weakmap with a proper way of storing private variables

--- utils/command.js
+++ utils/command.js
@@ -1,38 +1,46 @@
 import { MessageEmbed } from "discord.js";
 
-const embedMessageColors = [
-  "#0F52BA", // Sappire Blue
-  "#9b111e", // Ruby Red
-  "#50C878", // Emerald Green
-  "#faed27", // Yellow
-  "#E38C2D", // Orange
-  "#A1045A", // Magenta
-  "#9966cc", // Purple Amethyst
-  "#FFC0CB", // Pink
-  "#D5F6FB", // Aqua
-  "#9BD087", // Pastel Green
-  "#A4D8D8", // Pastel Cyan
-  "#FDFD96", // Pastel Yellow
-  "#AEC6CF", // Pastel Blue
-  "#C3B1E1", // Pastel Purple
-  "#FAC898", // Pastel Orange
-  "#E39FF6", // Lavender
-];
-const _mainColor = new WeakMap();
-
 class Command {
+  #embedMessageColors;
+  #mainColor;
+
   constructor() {
     this.MessageEmbed = MessageEmbed;
-    _mainColor.set(this, "#E0115F");
+    this.#embedMessageColors = [
+      "#0F52BA", // Sappire Blue
+      "#9b111e", // Ruby Red
+      "#50C878", // Emerald Green
+      "#faed27", // Yellow
+      "#E38C2D", // Orange
+      "#A1045A", // Magenta
+      "#9966cc", // Purple Amethyst
+      "#FFC0CB", // Pink
+      "#D5F6FB", // Aqua
+      "#9BD087", // Pastel Green
+      "#A4D8D8", // Pastel Cyan
+      "#FDFD96", // Pastel Yellow
+      "#AEC6CF", // Pastel Blue
+      "#C3B1E1", // Pastel Purple
+      "#FAC898", // Pastel Orange
+      "#E39FF6", // Lavender
+    ];
+    this.#mainColor = "#E0115F";
+  }
+
+  executeCommand(message) {
+    // This is to create safety and to work as kind of like an abstract method in OOP languages...
+    throw new Error(
+      "You forgot to add the implementation to your inherited command!"
+    );
   }
 
   get mainColor() {
-    return _mainColor.get(this);
+    return this.#mainColor;
   }
 
   getRandomEmbedColor() {
-    return embedMessageColors[
-      Math.floor(Math.random() * embedMessageColors.length)
+    return this.#embedMessageColors[
+      Math.floor(Math.random() * this.#embedMessageColors.length)
     ];
   }
 }
