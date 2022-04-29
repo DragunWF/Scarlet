c4bc97c0b9b3dd8cb1490d2d2b75fa338f6437b5
DragonWF <plarisanmarc@gmail.com>
2022-04-30 01:17:10 +0800

    Implemented error handling

--- commands/stats.js
+++ commands/stats.js
@@ -8,55 +8,61 @@ class StatsCommand extends Command {
   }
 
   async executeCommand(message, user) {
-    const stats = await this.#fetchCodeWarsStats(user);
-    const embed = new this.MessageEmbed()
-      .setColor(this.mainColor)
-      .setTitle("CodeWars Stats")
-      .setDescription(`**Username:** ${this.#formatStatValue(user)}`)
-      .setFields(
-        { name: "Skills", value: this.#concatenateUserSkills(stats.skills) },
-        {
-          name: "Rank",
-          value: this.#formatStatValue(stats.ranks.overall.name),
-          inline: true,
-        },
-        {
-          name: "Honor",
-          value: this.#formatStatValue(
-            Tools.formatNumberWithComma(stats.honor)
-          ),
-          inline: true,
-        },
-        {
-          name: "Clan",
-          value: this.#formatStatValue(this.#formatClan(stats.clan)),
-          inline: true,
-        },
-        {
-          name: "Katas Solved",
-          value: this.#formatStatValue(
-            Tools.formatNumberWithComma(stats.codeChallenges.totalCompleted)
-          ),
-          inline: true,
-        },
-        {
-          name: "Katas Authored",
-          value: this.#formatStatValue(
-            Tools.formatNumberWithComma(stats.codeChallenges.totalAuthored)
-          ),
-          inline: true,
-        },
-        {
-          name: "Leaderboard Position",
-          value: this.#formatStatValue(
-            Tools.formatNumberWithComma(stats.leaderboardPosition)
-          ),
-          inline: true,
-        }
-      )
-      .setFooter({ text: "Data fetched from CodeWars API" })
-      .setTimestamp();
-    message.channel.send({ embeds: [embed] });
+    try {
+      const stats = await this.#fetchCodeWarsStats(user);
+      const embed = new this.MessageEmbed()
+        .setColor(this.mainColor)
+        .setTitle("CodeWars Stats")
+        .setDescription(`**Username:** ${this.#formatStatValue(user)}`)
+        .setFields(
+          { name: "Skills", value: this.#concatenateUserSkills(stats.skills) },
+          {
+            name: "Rank",
+            value: this.#formatStatValue(stats.ranks.overall.name),
+            inline: true,
+          },
+          {
+            name: "Honor",
+            value: this.#formatStatValue(
+              Tools.formatNumberWithComma(stats.honor)
+            ),
+            inline: true,
+          },
+          {
+            name: "Clan",
+            value: this.#formatStatValue(this.#formatClan(stats.clan)),
+            inline: true,
+          },
+          {
+            name: "Katas Solved",
+            value: this.#formatStatValue(
+              Tools.formatNumberWithComma(stats.codeChallenges.totalCompleted)
+            ),
+            inline: true,
+          },
+          {
+            name: "Katas Authored",
+            value: this.#formatStatValue(
+              Tools.formatNumberWithComma(stats.codeChallenges.totalAuthored)
+            ),
+            inline: true,
+          },
+          {
+            name: "Leaderboard Position",
+            value: this.#formatStatValue(
+              Tools.formatNumberWithComma(stats.leaderboardPosition)
+            ),
+            inline: true,
+          }
+        )
+        .setFooter({ text: "Data fetched from CodeWars API" })
+        .setTimestamp();
+      message.channel.send({ embeds: [embed] });
+    } catch (error) {
+      message.channel.send(
+        "Sorry lad, but I don't know if that user exists..."
+      );
+    }
   }
 
   async #fetchCodeWarsStats(user) {
@@ -76,10 +82,7 @@ class StatsCommand extends Command {
   }
 
   #concatenateUserSkills(array) {
-    return array
-      .map((skill) => `\`${skill}\``)
-      .join(", ")
-      .trim();
+    return array.map((skill) => this.#formatStatValue(skill)).join(", ");
   }
 }
 
