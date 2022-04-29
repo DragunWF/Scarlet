c5779cbc01b81fb000d2f777267a1dadd2bb3999
DragonWF <plarisanmarc@gmail.com>
2022-04-30 01:03:38 +0800

    Added two new utility functions

--- utils/general-tools.js
+++ utils/general-tools.js
@@ -2,6 +2,15 @@ class Tools {
   static formatNumberWithComma(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+
+  static getRandomItemFromArray(array) {
+    if (!array.length) throw new Error("Array is empty!");
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  static getRandomInt(max) {
+    return Math.floor(Math.random * max);
+  }
 }
 
 export default Tools;
