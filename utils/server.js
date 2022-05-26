7903776b2eee32b0d5ed0dcb378929fbfc0bb4a4
DragonWF <plarisanmarc@gmail.com>
2022-05-27 00:39:52 +0800

    Updated port

--- utils/server.js
+++ utils/server.js
@@ -1,13 +1,15 @@
+import "dotenv/config";
 import express from "express";
 
 const server = express();
+const port = process.env.PORT || 8530;
 
 server.all("/", (request, response) => {
   response("Scarlet is now awake!");
 });
 
 function keepServerRunning() {
-  server.listen(3000, () => {
+  server.listen(port, () => {
     console.log("The server is now ready!");
   });
 }
