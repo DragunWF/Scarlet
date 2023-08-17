2efe6c1b0b636332884d1482d56c76bb95fcc1f5
dragunwf <plarisanmarc@gmail.com>
2023-08-17 15:49:03 +0800

    Create afk.js Checkpoint

---
+++ commands/afk.js
@@ -0,0 +1,12 @@
+import "fs";
+import Command from "../utils/command.js";
+
+class AwayCommand extends Command {
+  constructor() {
+    this.#users = {};
+  }
+
+  executeCommand(message, args) {
+    // incomplete
+  }
+}
