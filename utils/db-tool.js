c181656ec9d62ab89f58564f807f66234d0edc54
dragunwf <plarisanmarc@gmail.com>
2023-08-17 15:49:10 +0800

    Update db-tool.js Add comment

--- utils/db-tool.js
+++ utils/db-tool.js
@@ -7,7 +7,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE, // name of schema
 });
 console.log("Database has been connected successfully!");
 
