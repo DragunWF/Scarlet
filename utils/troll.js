dc94eb056d009ae70b64092e7f02849b6eb08cae
Marc Plarisan <plarisanmarc@gmail.com>
2023-09-15 10:03:47 +0800

    Update troll.js

--- utils/troll.js
+++ utils/troll.js
@@ -1,16 +1,21 @@
+import fs from "fs";
+
 class Troll {
   static #trollRole = "Citizen";
   static #rangeOfMinutes = [5, 10];
+  static #data = JSON.parse(fs.readFileSync("./config/bot.json"));
+  static #target;
 
-  static initializeTrolling(messageObj) {
-    this.#trollJewker(messageObj);
+  static initializeTrolling(messageObj, clientObj) {
+    this.#trollJewker(messageObj, clientObj);
+    this.#target = clientObj.users.fetch(this.#data.users.jewker);
     console.log("Trolling has been initialized!");
   }
 
   static #trollJewker(message) {
     const minutes = Math.floor(
-      Math.random() * (this.rangeOfMinutes[1] - this.rangeOfMinutes[0]) +
-        this.rangeOfMinutes[0]
+      Math.random() * (this.#rangeOfMinutes[1] - this.#rangeOfMinutes[0]) +
+        this.#rangeOfMinutes[0]
     );
     setTimeout(() => {
       message.guild.roles.find((role) => role.name === this.#trollRole);
