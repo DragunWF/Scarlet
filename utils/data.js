ff071706ad9df83e071d4b3d9ee045e633cbb96a
Marc Plarisan <plarisanmarc@gmail.com>
2023-09-15 10:14:11 +0800

    Update data.js

--- utils/data.js
+++ utils/data.js
@@ -1,5 +1,21 @@
-class Data {
+import fs from "fs";
 
+class Data {
+  static #settings = JSON.parse(fs.readFileSync("./config/bot.json"));
+  static #commands = JSON.parse(fs.readFileSync("./config/commands.json"));
+  static #keywords = JSON.parse(fs.readFileSync("./config/keywords.json"));
+
+  static getSettings() {
+    return this.#settings;
+  }
+
+  static getCommands() {
+    return this.#commands;
+  }
+
+  static getKeywords() {
+    return this.#keywords;
+  }
 }
 
 export default Data;
