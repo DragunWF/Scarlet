9c81385f80f29101a16ec43cf6f7f672e2f8e7ce
Marc Plarisan <plarisanmarc@gmail.com>
2023-09-23 12:56:02 +0800

    Update server.js

--- utils/server.js
+++ utils/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 
 const server = express();
-const port = process.env.PORT || 8530;
+const port = process.env.PORT || 8550;
 
 server.all("/", (req, res) => {
   res.send("Scarlet is now awake!");
